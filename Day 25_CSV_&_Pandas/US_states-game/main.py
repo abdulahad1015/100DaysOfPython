c6440ff7b8d80296e86613efb05d4767ac035f64
abdulahad1015 <abdulahad1015@gmail.com>
2024-07-05 16:37:22 +0500

    5/7/24

---
+++ Day 25_CSV_&_Pandas/US_states-game/main.py
@@ -0,0 +1,44 @@
+import turtle
+import csv
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+name_draw = turtle.Turtle(visible=False)
+name_draw.penup()
+name_draw.speed(0)
+
+data = pandas.read_csv("50_states.csv")
+
+not_guessed=[]
+all_states=data.state.tolist()
+score=0
+guessed_states=[]
+
+while(score<50):
+    answer_state = screen.textinput(title=f"{score} / 50", prompt="Whats another State ?")
+    answer_state=answer_state.title()
+    state = data[data["state"] == answer_state]
+    if(answer_state=="Exit"):
+        for i in all_states:
+            if i not in guessed_states:
+                not_guessed.append(i)
+        new_data=pandas.DataFrame(not_guessed)
+        new_data.to_csv("states_to_learn.csv")
+
+        print(not_guessed)
+        break
+    if(len(state)==0 or answer_state in guessed_states):
+        continue
+
+    score+=1
+    guessed_states.append(answer_state)
+    x = state.iloc[0].x
+    y = state.iloc[0].y
+    name_draw.goto(x,y)
+    name_draw.dot()
+    name_draw.write(arg=answer_state,font=("Arial",7,"normal"))
+
